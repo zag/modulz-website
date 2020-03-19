c029bea4fa026526ff93f9743468e1b6d7221208
Pedro Duarte <contact@peduarte.com>
2020-03-19 16:26:01 +0100

    Video player component

---
+++ components/VideoPlayer.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { AspectRatio, Box, Flex } from '@modulz/radix';
+
+type VideoPlayerProps = { id: string };
+
+export const VideoPlayer = ({ id, ...props }: VideoPlayerProps) => {
+  const [hasStartedPlaying, setHasStartedPlaying] = React.useState(false);
+  const [shouldUsePosterFallback, setShouldUsePosterFallback] = React.useState(false);
+  const [posterUrl, setPosterUrl] = React.useState(undefined);
+
+  React.useEffect(() => {
+    const img = new Image();
+    img.src = `http://i3.ytimg.com/vi/${id}/maxresdefault.jpg`;
+    img.onload = () => {
+      if (img.width <= 120) {
+        setPosterUrl(`http://i3.ytimg.com/vi/${id}/hqdefault.jpg`);
+        setShouldUsePosterFallback(true);
+      } else {
+        setPosterUrl(img.src);
+      }
+    };
+  }, []);
+
+  return (
+    <AspectRatio ratio="16:9" sx={{ overflow: 'hidden' }}>
+      <Flex
+        as="button"
+        aria-label="Play video"
+        onClick={() => setHasStartedPlaying(true)}
+        sx={{
+          appearance: 'none',
+          border: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+          bg: 'gray200',
+          ':hover, :focus': {
+            '[data-part-id="box"]': {
+              transform: 'scale(1.2)',
+            },
+          },
+        }}
+      >
+        {posterUrl && (
+          <img
+            src={posterUrl}
+            style={{
+              width: '100%',
+              height: '100%',
+              filter: shouldUsePosterFallback && 'blur(15px)',
+              transform: shouldUsePosterFallback && 'scale(2)',
+              willChange: 'filter',
+            }}
+          />
+        )}
+
+        <Box
+          sx={{
+            position: 'absolute',
+            boxSizing: 'content-box',
+            bg: 'blue600',
+            color: 'white',
+            borderRadius: '50%',
+            padding: 2,
+            svg: {
+              width: 5,
+              height: 5,
+            },
+            transform: hasStartedPlaying && 'scale(1.8) !important',
+            opacity: hasStartedPlaying && '0',
+            transition: hasStartedPlaying ? 'all 300ms' : 'all 100ms',
+          }}
+        >
+          <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 11L6 4L10.5 7.5L6 11Z" fill="currentColor" />
+          </svg>
+        </Box>
+      </Flex>
+
+      {hasStartedPlaying && (
+        <iframe
+          src={`https://www.youtube-nocookie.com/embed/${id}?autoplay=1&modestbranding=1`}
+          frameBorder={0}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ position: 'absolute', top: 0, left: 0, zIndex: 2, width: '100%', height: '100%' }}
+          {...props}
+        ></iframe>
+      )}
+    </AspectRatio>
+  );
+};
