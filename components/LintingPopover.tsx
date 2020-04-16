2fb6ce5f9ef53b97956d5dc1626773b7209cdaff
Pedro Duarte <contact@peduarte.com>
2020-04-16 13:54:25 +0200

    Add scroller

--- components/LintingPopover.tsx
+++ components/LintingPopover.tsx
@@ -5,16 +5,20 @@ export const LintingPopover = () => {
   return (
     <Flex>
       <Box>
-        <Box sx={{
-          flexShrink: '0',
-          backgroundColor: 'white',
-          borderRadius: '2',
-          position: 'relative',
-          boxShadow: '0 10px 38px -10px rgba(22,23,24,0.35), 0 10px 20px -15px rgba(22,23,24,0.2)',
-        }}>
-          <Box sx={{
-            padding: '4',
-          }}>
+        <Box
+          sx={{
+            flexShrink: 0,
+            backgroundColor: 'white',
+            borderRadius: '2',
+            position: 'relative',
+            boxShadow: '0 10px 38px -10px rgba(22,23,24,0.35), 0 10px 20px -15px rgba(22,23,24,0.2)',
+          }}
+        >
+          <Box
+            sx={{
+              padding: '4',
+            }}
+          >
             <Text size={2} weight="medium" mb={1} sx={{ display: 'block' }} style={{ lineHeight: '1' }}>
               Font size too small
             </Text>
@@ -22,18 +26,16 @@ export const LintingPopover = () => {
               Modulz Styleguide provides fully-featured design system documentation for teams who need total control.
             </Text>
             <Flex>
-              <Button mr={2}>
-                Ignore
-              </Button>
-              <Button variant="blue">
-                Fix
-              </Button>
+              <Button mr={2}>Ignore</Button>
+              <Button variant="blue">Fix</Button>
             </Flex>
           </Box>
-          <Box sx={{
-            padding: '4',
-            pt: '1'
-          }}>
+          <Box
+            sx={{
+              padding: '4',
+              pt: '1',
+            }}
+          >
             <Text size={2} weight="medium" mb={1} sx={{ display: 'block' }} style={{ lineHeight: '1' }}>
               Font size too small
             </Text>
@@ -41,18 +43,16 @@ export const LintingPopover = () => {
               Modulz Styleguide provides fully-featured design system documentation for teams who need total control.
             </Text>
             <Flex>
-              <Button mr={2}>
-                Ignore
-              </Button>
-              <Button variant="blue">
-                Fix
-              </Button>
+              <Button mr={2}>Ignore</Button>
+              <Button variant="blue">Fix</Button>
             </Flex>
           </Box>
-          <Box sx={{
-            padding: '4',
-            pt: '1'
-          }}>
+          <Box
+            sx={{
+              padding: '4',
+              pt: '1',
+            }}
+          >
             <Text size={2} weight="medium" mb={1} sx={{ display: 'block' }} style={{ lineHeight: '1' }}>
               Font size too small
             </Text>
@@ -60,21 +60,54 @@ export const LintingPopover = () => {
               Modulz Styleguide provides fully-featured design system documentation for teams who need total control.
             </Text>
             <Flex>
-              <Button mr={2}>
-                Ignore
-              </Button>
-              <Button variant="blue">
-                Fix
-              </Button>
+              <Button mr={2}>Ignore</Button>
+              <Button variant="blue">Fix</Button>
             </Flex>
           </Box>
         </Box>
       </Box>
       <Box>
-        <Flex ml={4} sx={{ position: 'relative', alignItems: 'center', justifyContent: 'center', fontSize: '4', width: '6', height: '6', backgroundColor: 'yellow600', borderRadius: '50%', color: 'black' }}>
+        <Flex
+          ml={4}
+          sx={{
+            position: 'relative',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: '4',
+            width: '6',
+            height: '6',
+            backgroundColor: 'yellow600',
+            borderRadius: '50%',
+            color: 'black',
+          }}
+        >
           3
-          <Box sx={{ position: 'absolute', top: '0', left: '0', width: '6', height: '6', border: '1px solid', borderColor: 'yellow300', borderRadius: '50%', transform: 'scale(1.4)' }}></Box>
-          <Box sx={{ position: 'absolute', top: '0', left: '0', width: '6', height: '6', border: '1px solid', borderColor: 'yellow200', borderRadius: '50%', transform: 'scale(1.8)' }}></Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              width: '6',
+              height: '6',
+              border: '1px solid',
+              borderColor: 'yellow300',
+              borderRadius: '50%',
+              transform: 'scale(1.4)',
+            }}
+          ></Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              width: '6',
+              height: '6',
+              border: '1px solid',
+              borderColor: 'yellow200',
+              borderRadius: '50%',
+              transform: 'scale(1.8)',
+            }}
+          ></Box>
         </Flex>
       </Box>
     </Flex>
