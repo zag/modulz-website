2b36c1b09ea06983e5eb8329cde8c5f6cf7899fd
Colm Tuite <colmtuite@gmail.com>
2021-01-09 03:23:39 +0000

    idk what this is

--- pages/design.tsx
+++ pages/design.tsx
@@ -607,20 +607,117 @@ const DesignPage = () => {
       <Divider mx="auto" size={2} />
 
       <Box sx={{ position: 'relative', p: 9 }}>
-        <Container size={2}>
-          <Grid
-            sx={{
-              gridTemplateColumns: ['repeat(1, 1fr)', 'repeat(2, 1fr)'],
-              gap: 8,
-              alignItems: 'center',
-            }}
-          >
-            <Box sx={{ alignSelf: 'stretch' }}>
-              <Flex sx={{ justifyContent: 'space-between' }}>
-                <Box sx={{ height: '100%' }}>
-                  <Box sx={{ backgroundImage: 'linear-gradient(0deg, hsl(270,75%,45%) 0%, hsl(270,75%,60%) 100%)', border: '1px solid hsl(270,75%,40%)', boxShadow: '0 3px 10px rgba(0,0,0,.25)', color: 'white', fontSize: 3, height: '35px', borderRadius: '9999px', px: 4, fontWeight: '500', display: 'inline-flex', alignItems: 'center' }} style={{ lineHeight: '1' }}>Button</Box>
+        <Container size={3}>
+          <Flex>
+            <Box sx={{ flexGrow: 1, flexBasis: 0 }}>
+              <Text size={1} sx={{ fontWeight: '500' }}>Variants</Text>
+            </Box>
+            <Box sx={{ width: '250px', flexShrink: 0, mx: 8 }}>
+                <Box sx={{ boxShadow: '0 15px 35px -10px rgba(0,0,0,.35)', borderRadius: 2 }}>
+                <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', pt: 1, px: 3 }}>
+                  <Text size={1} sx={{ fontWeight: '500' }}>Variants</Text>
+                  <Box sx={{ mr: -2 }}>
+                    <IconButton>
+                      <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M8 3C8 2.72386 7.77614 2.5 7.5 2.5C7.22386 2.5 7 2.72386 7 3V7H3C2.72386 7 2.5 7.22386 2.5 7.5C2.5 7.77614 2.72386 8 3 8H7V12C7 12.2761 7.22386 12.5 7.5 12.5C7.77614 12.5 8 12.2761 8 12V8H12C12.2761 8 12.5 7.77614 12.5 7.5C12.5 7.22386 12.2761 7 12 7H8V3Z" fill="currentColor"/>
+                      </svg>
+                    </IconButton>
+                  </Box>
+                </Flex>
+                <Box sx={{ py: 1 }}>
+                  <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', py: 1, px: 3, backgroundColor: 'blue600' }}>
+                    <Box>
+                      <Text size={1} sx={{ display: 'block', pt: '3px', pr: '4px', pb: '3px', pl: '2px', backgroundColor: 'blue700', color: 'white', fontFamily: 'Söhne Mono' }} style={{ lineHeight: 1 }}>:base</Text>
+                    </Box>
+                    <Flex sx={{ alignItems: 'center', mr: -2 }}>
+                      <Text size={1} sx={{ mr: 1, color: 'white' }} style={{ lineHeight: 1 }}>(27)</Text>
+                      <IconButton>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 4.35355C11.5488 4.15829 11.5488 3.84171 11.3536 3.64645C11.1583 3.45118 10.8417 3.45118 10.6464 3.64645L7.5 6.79289L4.35355 3.64645C4.15829 3.45118 3.84171 3.45118 3.64645 3.64645C3.45118 3.84171 3.45118 4.15829 3.64645 4.35355L6.79289 7.5L3.64645 10.6464C3.45118 10.8417 3.45118 11.1583 3.64645 11.3536C3.84171 11.5488 4.15829 11.5488 4.35355 11.3536L7.5 8.20711L10.6464 11.3536C10.8417 11.5488 11.1583 11.5488 11.3536 11.3536C11.5488 11.1583 11.5488 10.8417 11.3536 10.6464L8.20711 7.5L11.3536 4.35355Z" fill="currentColor"/>
+                        </svg>
+                      </IconButton>
+                    </Flex>
+                  </Flex>
+                  <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', py: 1, px: 3 }}>
+                    <Box>
+                      <Text size={1} sx={{ display: 'block', pt: '3px', pr: '4px', pb: '3px', pl: '2px', backgroundColor: 'blue200', color: 'blue700', fontFamily: 'Söhne Mono' }} style={{ lineHeight: 1 }}>:hover</Text>
+                    </Box>
+                    <Flex sx={{ alignItems: 'center', mr: -2 }}>
+                      <Text size={1} sx={{ mr: 1, color: 'gray700' }} style={{ lineHeight: 1 }}>(27)</Text>
+                      <IconButton>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 4.35355C11.5488 4.15829 11.5488 3.84171 11.3536 3.64645C11.1583 3.45118 10.8417 3.45118 10.6464 3.64645L7.5 6.79289L4.35355 3.64645C4.15829 3.45118 3.84171 3.45118 3.64645 3.64645C3.45118 3.84171 3.45118 4.15829 3.64645 4.35355L6.79289 7.5L3.64645 10.6464C3.45118 10.8417 3.45118 11.1583 3.64645 11.3536C3.84171 11.5488 4.15829 11.5488 4.35355 11.3536L7.5 8.20711L10.6464 11.3536C10.8417 11.5488 11.1583 11.5488 11.3536 11.3536C11.5488 11.1583 11.5488 10.8417 11.3536 10.6464L8.20711 7.5L11.3536 4.35355Z" fill="currentColor"/>
+                        </svg>
+                      </IconButton>
+                    </Flex>
+                  </Flex>
+                  <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', py: 1, px: 3 }}>
+                    <Box>
+                      <Text size={1} sx={{ display: 'block', pt: '3px', pr: '4px', pb: '3px', pl: '2px', backgroundColor: 'blue200', color: 'blue700', fontFamily: 'Söhne Mono' }} style={{ lineHeight: 1 }}>:active</Text>
+                    </Box>
+                    <Flex sx={{ alignItems: 'center', mr: -2 }}>
+                      <Text size={1} sx={{ mr: 1, color: 'gray700' }} style={{ lineHeight: 1 }}>(27)</Text>
+                      <IconButton>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 4.35355C11.5488 4.15829 11.5488 3.84171 11.3536 3.64645C11.1583 3.45118 10.8417 3.45118 10.6464 3.64645L7.5 6.79289L4.35355 3.64645C4.15829 3.45118 3.84171 3.45118 3.64645 3.64645C3.45118 3.84171 3.45118 4.15829 3.64645 4.35355L6.79289 7.5L3.64645 10.6464C3.45118 10.8417 3.45118 11.1583 3.64645 11.3536C3.84171 11.5488 4.15829 11.5488 4.35355 11.3536L7.5 8.20711L10.6464 11.3536C10.8417 11.5488 11.1583 11.5488 11.3536 11.3536C11.5488 11.1583 11.5488 10.8417 11.3536 10.6464L8.20711 7.5L11.3536 4.35355Z" fill="currentColor"/>
+                        </svg>
+                      </IconButton>
+                    </Flex>
+                  </Flex>
+                  <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', py: 1, px: 3 }}>
+                    <Box>
+                      <Text size={1} sx={{ display: 'block', pt: '3px', pr: '4px', pb: '3px', pl: '2px', backgroundColor: 'blue200', color: 'blue700', fontFamily: 'Söhne Mono' }} style={{ lineHeight: 1 }}>:focus</Text>
+                    </Box>
+                    <Flex sx={{ alignItems: 'center', mr: -2 }}>
+                      <Text size={1} sx={{ mr: 1, color: 'gray700' }} style={{ lineHeight: 1 }}>(27)</Text>
+                      <IconButton>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 4.35355C11.5488 4.15829 11.5488 3.84171 11.3536 3.64645C11.1583 3.45118 10.8417 3.45118 10.6464 3.64645L7.5 6.79289L4.35355 3.64645C4.15829 3.45118 3.84171 3.45118 3.64645 3.64645C3.45118 3.84171 3.45118 4.15829 3.64645 4.35355L6.79289 7.5L3.64645 10.6464C3.45118 10.8417 3.45118 11.1583 3.64645 11.3536C3.84171 11.5488 4.15829 11.5488 4.35355 11.3536L7.5 8.20711L10.6464 11.3536C10.8417 11.5488 11.1583 11.5488 11.3536 11.3536C11.5488 11.1583 11.5488 10.8417 11.3536 10.6464L8.20711 7.5L11.3536 4.35355Z" fill="currentColor"/>
+                        </svg>
+                      </IconButton>
+                    </Flex>
+                  </Flex>
+                  <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', py: 1, px: 3 }}>
+                    <Box>
+                      <Text size={1} sx={{ display: 'block', pt: '3px', pr: '4px', pb: '3px', pl: '2px', backgroundColor: 'blue200', color: 'blue700', fontFamily: 'Söhne Mono' }} style={{ lineHeight: 1 }}>:active:hover</Text>
+                    </Box>
+                    <Flex sx={{ alignItems: 'center', mr: -2 }}>
+                      <Text size={1} sx={{ mr: 1, color: 'gray700' }} style={{ lineHeight: 1 }}>(27)</Text>
+                      <IconButton>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 4.35355C11.5488 4.15829 11.5488 3.84171 11.3536 3.64645C11.1583 3.45118 10.8417 3.45118 10.6464 3.64645L7.5 6.79289L4.35355 3.64645C4.15829 3.45118 3.84171 3.45118 3.64645 3.64645C3.45118 3.84171 3.45118 4.15829 3.64645 4.35355L6.79289 7.5L3.64645 10.6464C3.45118 10.8417 3.45118 11.1583 3.64645 11.3536C3.84171 11.5488 4.15829 11.5488 4.35355 11.3536L7.5 8.20711L10.6464 11.3536C10.8417 11.5488 11.1583 11.5488 11.3536 11.3536C11.5488 11.1583 11.5488 10.8417 11.3536 10.6464L8.20711 7.5L11.3536 4.35355Z" fill="currentColor"/>
+                        </svg>
+                      </IconButton>
+                    </Flex>
+                  </Flex>
+                  <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', py: 1, px: 3 }}>
+                    <Box>
+                      <Text size={1} sx={{ display: 'block', pt: '3px', pr: '4px', pb: '3px', pl: '2px', backgroundColor: 'blue200', color: 'blue700', fontFamily: 'Söhne Mono' }} style={{ lineHeight: 1 }}>:disabled</Text>
+                    </Box>
+                    <Flex sx={{ alignItems: 'center', mr: -2 }}>
+                      <Text size={1} sx={{ mr: 1, color: 'gray700' }} style={{ lineHeight: 1 }}>(27)</Text>
+                      <IconButton>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 4.35355C11.5488 4.15829 11.5488 3.84171 11.3536 3.64645C11.1583 3.45118 10.8417 3.45118 10.6464 3.64645L7.5 6.79289L4.35355 3.64645C4.15829 3.45118 3.84171 3.45118 3.64645 3.64645C3.45118 3.84171 3.45118 4.15829 3.64645 4.35355L6.79289 7.5L3.64645 10.6464C3.45118 10.8417 3.45118 11.1583 3.64645 11.3536C3.84171 11.5488 4.15829 11.5488 4.35355 11.3536L7.5 8.20711L10.6464 11.3536C10.8417 11.5488 11.1583 11.5488 11.3536 11.3536C11.5488 11.1583 11.5488 10.8417 11.3536 10.6464L8.20711 7.5L11.3536 4.35355Z" fill="currentColor"/>
+                        </svg>
+                      </IconButton>
+                    </Flex>
+                  </Flex>
+                  <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', py: 1, px: 3 }}>
+                    <Flex>
+                      <Text size={1} sx={{ display: 'block', pt: '3px', pr: '4px', pb: '3px', pl: '2px', backgroundColor: 'blue200', color: 'blue700', fontFamily: 'Söhne Mono', mr: 1 }} style={{ lineHeight: 1 }}>:hover</Text>
+                      <Text size={1} sx={{ display: 'block', pt: '3px', pr: '4px', pb: '3px', pl: '2px', backgroundColor: 'blue200', color: 'blue700', fontFamily: 'Söhne Mono' }} style={{ lineHeight: 1 }}>:icon</Text>
+                    </Flex>
+                    <Flex sx={{ alignItems: 'center', mr: -2 }}>
+                      <Text size={1} sx={{ mr: 1, color: 'gray700' }} style={{ lineHeight: 1 }}>(27)</Text>
+                      <IconButton>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 4.35355C11.5488 4.15829 11.5488 3.84171 11.3536 3.64645C11.1583 3.45118 10.8417 3.45118 10.6464 3.64645L7.5 6.79289L4.35355 3.64645C4.15829 3.45118 3.84171 3.45118 3.64645 3.64645C3.45118 3.84171 3.45118 4.15829 3.64645 4.35355L6.79289 7.5L3.64645 10.6464C3.45118 10.8417 3.45118 11.1583 3.64645 11.3536C3.84171 11.5488 4.15829 11.5488 4.35355 11.3536L7.5 8.20711L10.6464 11.3536C10.8417 11.5488 11.1583 11.5488 11.3536 11.3536C11.5488 11.1583 11.5488 10.8417 11.3536 10.6464L8.20711 7.5L11.3536 4.35355Z" fill="currentColor"/>
+                        </svg>
+                      </IconButton>
+                    </Flex>
+                  </Flex>
                 </Box>
-                <Box sx={{ width: '250px', boxShadow: '0 15px 35px -10px rgba(0,0,0,.35)', borderRadius: 2 }}>
                   <Flex sx={{ alignItems: 'center', justifyContent: 'space-between', pt: 1, px: 3 }}>
                     <Text size={1} sx={{ fontWeight: '500' }}>Selector</Text>
                     <Box sx={{ mr: -2 }}>
@@ -727,94 +824,16 @@ const DesignPage = () => {
                   </Box>
                 </Box>
 
-              </Flex>
             </Box>
-            <Box>
-              <Text
-                as="h3"
-                size={2}
-                mb={4}
-                sx={{
-                  color: 'blue700',
-                  letterSpacing: '.1em',
-                  textTransform: 'uppercase',
-                  fontFamily: 'Söhne Mono'
-                }}
-              >
-                &lt;States/&gt;
-              </Text>
+            <Box sx={{ flexGrow: 1, flexBasis: 0 }}>
               <Heading as="h3" size={3} mb={3} sx={{ fontWeight: 500 }}>
                 States
               </Heading>
               <Text as="p" size={4} sx={{ lineHeight: '3' }}>
                 Modulz Styleguide provides fully-featured design system documentation for teams who need total control.
               </Text>
-              <Flex sx={{ pt: 3 }}>
-                <Box>
-                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
-<rect width="45" height="45" fill="white"/>
-<path d="M8.5 4V41" stroke="#FFC5FD"/>
-<path d="M36.5 4V41" stroke="#FFC5FD"/>
-<path d="M41 8.5H4" stroke="#FFC5FD"/>
-<path d="M41 36.5H4" stroke="#FFC5FD"/>
-<rect x="9" y="9" width="11" height="11" fill="#FF00C7"/>
-<rect x="9" y="25" width="11" height="11" fill="#FF00C7"/>
-<rect x="25" y="25" width="11" height="11" fill="#FF00C7"/>
-<path d="M25.5 13V16" stroke="#FF00C7"/>
-<path d="M35.5 13V16" stroke="#FF00C7"/>
-<path d="M25.5 18.5V19.5H26.5" stroke="#FF00C7" stroke-linecap="square"/>
-<path d="M34.5 19.5H35.5V18.5" stroke="#FF00C7" stroke-linecap="square"/>
-<path d="M26.5 9.5H25.5V10.5" stroke="#FF00C7" stroke-linecap="square"/>
-<path d="M35.5 10.5V9.5H34.5" stroke="#FF00C7" stroke-linecap="square"/>
-<path d="M32 19.5H29" stroke="#FF00C7"/>
-<path d="M32 9.5H29" stroke="#FF00C7"/>
-</svg>
-                </Box>
-                <Box>
-                  <Text as="p" size={3} sx={{ fontWeight: '500', lineHeight: '2', mt: 2, ml: 2 }}>
-                    Compound states
-                  </Text>
-                </Box>
-              </Flex>
-              <Box sx={{ pl: 7 }}>
-                <Text as="p" size={3} sx={{ color: 'gray700', lineHeight: '2', ml: 2 }}>
-                  Modulz Styleguide provides fully-featured design system documentation.
-                </Text>
-              </Box>
-              <Flex sx={{ pt: 3 }}>
-                <Box>
-                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
-<rect width="45" height="45" fill="white"/>
-<path d="M8.5 4V41" stroke="#FFC5FD"/>
-<path d="M36.5 4V41" stroke="#FFC5FD"/>
-<path d="M41 8.5H4" stroke="#FFC5FD"/>
-<path d="M41 36.5H4" stroke="#FFC5FD"/>
-<rect x="9" y="9" width="11" height="11" fill="#FF00C7"/>
-<rect x="9" y="25" width="11" height="11" fill="#FF00C7"/>
-<rect x="25" y="25" width="11" height="11" fill="#FF00C7"/>
-<path d="M25.5 13V16" stroke="#FF00C7"/>
-<path d="M35.5 13V16" stroke="#FF00C7"/>
-<path d="M25.5 18.5V19.5H26.5" stroke="#FF00C7" stroke-linecap="square"/>
-<path d="M34.5 19.5H35.5V18.5" stroke="#FF00C7" stroke-linecap="square"/>
-<path d="M26.5 9.5H25.5V10.5" stroke="#FF00C7" stroke-linecap="square"/>
-<path d="M35.5 10.5V9.5H34.5" stroke="#FF00C7" stroke-linecap="square"/>
-<path d="M32 19.5H29" stroke="#FF00C7"/>
-<path d="M32 9.5H29" stroke="#FF00C7"/>
-</svg>
-                </Box>
-                <Box>
-                  <Text as="p" size={3} sx={{ fontWeight: '500', lineHeight: '2', mt: 2, ml: 2 }}>
-                    Child selectors
-                  </Text>
-                </Box>
-              </Flex>
-              <Box sx={{ pl: 7 }}>
-                <Text as="p" size={3} sx={{ color: 'gray700', lineHeight: '2', ml: 2 }}>
-                  Modulz Styleguide provides fully-featured design system documentation.
-                </Text>
-              </Box>
             </Box>
-          </Grid>
+          </Flex>
         </Container>
       </Box>
 
