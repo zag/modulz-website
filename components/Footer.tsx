ffe48d8ca083fe5d17bfd2a65d2264b235569077
Colm Tuite <colmtuite@gmail.com>
2021-03-23 05:06:28 +0000

    Fix up footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,143 +1,156 @@
 import React from 'react';
 import NextLink from 'next/link';
-import { Box, Container, Grid, Heading, Text, Link, Flex, Separator } from '@modulz/design-system';
+import { Box, Section, Container, Grid, Subheading, Heading, Text, Link, Flex, Separator } from '@modulz/design-system';
 import { Logo } from './Logo';
 
 export const Footer = () => {
   return (
-    <Box>
+    <Section>
       <Separator size="1" />
       <Container size="3">
-        <Flex>
-          <Flex css={{ alignItems: 'center' }}>
+        <Grid css={{ gridTemplateColumns: '1fr 1fr 1fr 1fr', gap: '$7' }}>
+          <Flex css={{ fd: 'column', jc: 'space-between' }}>
             <NextLink href="/" passHref>
-              <Link variant="subtle" css={{ color: 'inherit', ':focus': { boxShadow: 'none' } }}>
-                <Box>Modulz homepage</Box>
+              <Box as="a" css={{ color: 'inherit' }}>
                 <Logo aria-hidden />
-              </Link>
+              </Box>
             </NextLink>
-
             <Text
-              as="span"
               size="2"
-              css={{ color: 'gray700', lineHeight: '0', userSelect: 'none' }}
+              css={{ color: '$gray800', }}
             >
               &copy; Modulz, Inc
             </Text>
           </Flex>
           <Box>
-            <Heading>
+            <Subheading>
               Product
-            </Heading>
-            <ul>
-              <li>
-                <Text as="p" size="3" css={{ color: 'gray500', lineHeight: '1', userSelect: 'none' }}>
-                  Design
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ color: 'gray500', lineHeight: '1', userSelect: 'none' }}>
-                  Prototyping
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ color: 'gray500', lineHeight: '1', userSelect: 'none' }}>
-                  Handoff
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ color: 'gray500', lineHeight: '1', userSelect: 'none' }}>
-                  Design systems
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+            </Subheading>
+            <Box as="ul" css={{ listStyle: 'none', m: 0, p: 0 }}>
+              <Box as="li" css={{ my: '$3' }}>
+                <NextLink href="/design" passHref>
+                  <Link variant="subtle">
+                    <Text size="3">
+                      Design
+                    </Text>
+                  </Link>
+                </NextLink>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <NextLink href="/prototyping" passHref>
+                  <Link variant="subtle">
+                    <Text size="3">
+                      Prototyping
+                    </Text>
+                  </Link>
+                </NextLink>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <NextLink href="/design" passHref>
+                  <Link variant="subtle">
+                    <Text size="3">
+                      Handoff
+                    </Text>
+                  </Link>
+                </NextLink>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <NextLink href="/design" passHref>
+                  <Link variant="subtle">
+                    <Text size="3">
+                      Design Systems
+                    </Text>
+                  </Link>
+                </NextLink>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{ }}>
                   <Link variant="subtle" href="https://radix.modulz.app">
                     Radix
                   </Link>
                 </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{ }}>
                   <Link variant="subtle" href="https://stitches.dev">
                     Stitches
                   </Link>
                 </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{  }}>
                   <Link variant="subtle" href="https://modulz.typeform.com/to/fp4gBW">
                     Beta
                   </Link>
                 </Text>
-              </li>
-            </ul>
+              </Box>
+            </Box>
           </Box>
           <Box>
-            <Heading>
+            <Subheading>
               Community
-            </Heading>
-            <ul>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+            </Subheading>
+            <Box as="ul" css={{ listStyle: 'none', m: 0, p: 0 }}>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{  }}>
                   <Link variant="subtle" href="https://twitter.com/modulz">
                     Twitter
                   </Link>
                 </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{  }}>
                   <Link variant="subtle" href="https://github.com/modulz">
                     Github
                   </Link>
                 </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{  }}>
                   <Link variant="subtle" href="https://discord.gg/p8PwAPFNHc">
                     Discord
                   </Link>
                 </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{  }}>
                   <Link variant="subtle" href="https://www.youtube.com/channel/UCEU5U-sfF6fmGvTmelBRePQ">
                     Youtube
                   </Link>
                 </Text>
-              </li>
-            </ul>
+              </Box>
+            </Box>
           </Box>
           <Box>
-            <Heading>
+            <Subheading>
               Company
-            </Heading>
-            <ul>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+            </Subheading>
+            <Box as="ul" css={{ listStyle: 'none', m: 0, p: 0 }}>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{  }}>
                   <NextLink href="/about" passHref>
                     <Link variant="subtle">About</Link>
                   </NextLink>
                 </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{  }}>
                   <NextLink href="/blog" passHref>
                     <Link variant="subtle">Blog</Link>
                   </NextLink>
                 </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ lineHeight: '1' }}>
+              </Box>
+              <Box as="li" css={{ my: '$3' }}>
+                <Text as="p" size="3" css={{  }}>
                   <NextLink href="/careers" passHref>
                     <Link variant="subtle">Careers</Link>
                   </NextLink>
                 </Text>
-              </li>
-            </ul>
+              </Box>
+            </Box>
           </Box>
-        </Flex>
+        </Grid>
       </Container>
-    </Box>
+    </Section>
   );
 };
